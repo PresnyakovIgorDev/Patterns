3514e4d7018a65dfaaeea692459f75c77a1eb841
Igor <ig.presnyakov@gmail.com>
2019-11-07 13:22:38 +0300

    Singleton

---
+++ Patterns/Creational/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.Creational
+{
+    /// <summary>
+    /// Одиночка (Singleton, Синглтон) - порождающий паттерн, который гарантирует, что для определенного класса будет создан только один объект, а также предоставит к этому объекту точку доступа.
+    /// </summary>
+    public class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+
+        public string TestProperty { get; set; }
+
+        private Singleton()
+        {
+            TestProperty = "Test";
+        }
+
+        /// <summary>
+        /// Получить инстанс Singleton
+        /// </summary>
+        public static Singleton Instance => _instance;
+    }
+}
---
+++ PatternsExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Patterns.Creational;
+
+namespace PatternsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.TestProperty);
+            Console.WriteLine(Singleton.Instance.TestProperty);
+
+            Console.ReadLine();
+        }
+    }
+
+}
